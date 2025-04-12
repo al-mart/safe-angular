c0f485608a340c6f6aa09269f7e0ad3d4028ef4b
Aleksandr Martirosyan <aleksandr.martirosyan@gmail.com>
2025-04-12 22:21:03 +0400

    feat: updated lock

--- libs/http/src/lib/utils/is-absolute-path.ts
+++ libs/http/src/lib/utils/is-absolute-path.ts
@@ -1,5 +1,4 @@
 export function isAbsolutePath(path: string, matchRegExp: RegExp = /^(http|https):\/\//): boolean {
-  // eslint-disable-next-line @cspell/spellchecker
   // eslint-disable-next-line sonarjs/prefer-regexp-exec
   const result: string[] | null = path.match(matchRegExp);
 
